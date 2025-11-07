3b426ea264325cbde4d5a2b485adff7fbfaca468
Mike AUBENAS <mike.aubenas@gmail.com>
2025-11-07 19:25:55 +0100

    feat: tab to circle to next cell

--- src/core/events.rs
+++ src/core/events.rs
@@ -12,10 +12,14 @@ pub fn handle_keyboard_events(state: &mut State) -> io::Result<bool> {
 
     if let Event::Key(key) = event {
         match key.code {
+            // Moving
             KeyCode::Up | KeyCode::Char('k') => state.move_cell_top(),
             KeyCode::Down | KeyCode::Char('j') => state.move_cell_bottom(),
             KeyCode::Left | KeyCode::Char('h') => state.move_cell_left(),
             KeyCode::Right | KeyCode::Char('l') => state.move_cell_right(),
+            KeyCode::Tab => state.move_next_cell(),
+
+            // Filling
             KeyCode::Char(c) if c.is_ascii_digit() => {
                 if let Some(d) = c.to_digit(10) {
                     state.set_number(d as u8);
@@ -23,6 +27,7 @@ pub fn handle_keyboard_events(state: &mut State) -> io::Result<bool> {
             }
             KeyCode::Backspace | KeyCode::Delete | KeyCode::Char('0') => state.clear_cell(),
 
+            // App
             KeyCode::Char('q') | KeyCode::Esc => return Ok(true),
             _ => {}
         }
--- src/core/state.rs
+++ src/core/state.rs
@@ -1,6 +1,10 @@
 use std::usize;
 
-use sabita::core::{constants::LENGTH_DIMENSION, grid::Grid, validation::validate};
+use sabita::core::{
+    constants::{LENGTH_DIMENSION, TO_BE_SOLVED},
+    grid::{BoxLocation, Grid},
+    validation::validate,
+};
 
 ////////////////////////////////////////
 
@@ -26,12 +30,13 @@ impl State {
     pub fn new() -> State {
         let grid = Grid::generate(Some(BASE_MISSING_VALUES));
 
+        let BoxLocation { line, column, .. } = grid.locate_missing_box()[0];
         State {
             grid_to_solve: grid.clone(),
             original_grid: grid.clone(),
 
-            cursor_row: 0,
-            cursor_col: 0,
+            cursor_row: line,
+            cursor_col: column,
 
             original_nb_missing_values: BASE_MISSING_VALUES,
             remaining_nb_missing_values: BASE_MISSING_VALUES,
@@ -45,7 +50,7 @@ impl State {
         for col_index in (0..self.cursor_col).rev() {
             let cell = self.original_grid.values[self.cursor_row][col_index];
 
-            if cell == 0 {
+            if cell == TO_BE_SOLVED {
                 self.cursor_col = col_index;
                 break;
             }
@@ -97,11 +102,54 @@ impl State {
         }
     }
 
+    pub fn move_next_cell(&mut self) {
+        let missing_cells = self.original_grid.locate_missing_box();
+
+        for (index, cell) in missing_cells.iter().enumerate() {
+            let BoxLocation { line, column, .. } = cell;
+
+            let is_current_cell = *line == self.cursor_row && *column == self.cursor_col;
+            if !is_current_cell {
+                continue;
+            }
+
+            let is_next_available = index + 1 < missing_cells.len();
+
+            if is_next_available {
+                let BoxLocation {
+                    line: next_row,
+                    column: next_col,
+                    ..
+                } = missing_cells[index + 1];
+
+                self.cursor_row = next_row;
+                self.cursor_col = next_col;
+
+                return;
+            }
+        }
+
+        // If not found before, go back to first available cell
+        let BoxLocation { line, column, .. } = missing_cells[0];
+
+        self.cursor_row = line;
+        self.cursor_col = column;
+    }
     pub fn set_number(&mut self, num: u8) {
         if num >= 1 && num <= LENGTH_DIMENSION {
+            let previous_value = self.grid_to_solve.values[self.cursor_row][self.cursor_col];
             self.grid_to_solve.values[self.cursor_row][self.cursor_col] = num;
 
-            self.remaining_nb_missing_values -= 1;
+            if previous_value == TO_BE_SOLVED {
+                self.remaining_nb_missing_values -= 1;
+
+                if self.remaining_nb_missing_values == 0 {
+                    match validate(&self.grid_to_solve.values) {
+                        Ok(_) => self.is_solved = Some(true),
+                        Err(_) => self.is_solved = Some(false),
+                    }
+                }
+            }
         }
     }
 
@@ -113,10 +161,14 @@ impl State {
     }
 
     fn reset_state(&mut self) {
-        self.grid_to_solve = self.original_grid.clone();
+        let base_grid = self.original_grid.clone();
 
-        self.cursor_row = 0;
-        self.cursor_col = 0;
+        let BoxLocation { line, column, .. } = base_grid.locate_missing_box()[0];
+
+        self.grid_to_solve = base_grid;
+
+        self.cursor_row = line;
+        self.cursor_col = column;
 
         self.remaining_nb_missing_values = self.original_nb_missing_values;
     }
