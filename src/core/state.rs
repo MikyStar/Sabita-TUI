144457eb7f9f6c5df6fb78d3a002b3c397f87a98
Mike AUBENAS <mike.aubenas@gmail.com>
2025-11-07 17:18:12 +0100

    feat: use sabita to fill grid + better colors

--- src/core/state.rs
+++ src/core/state.rs
@@ -1,31 +1,47 @@
-use sabita::core::constants::LENGTH_DIMENSION;
+use sabita::core::{
+    constants::LENGTH_DIMENSION,
+    grid::{BoxLocation, Grid, GridValues},
+    validation::validate,
+};
 
 ////////////////////////////////////////
 
 pub const LENGTH_USIZE: usize = LENGTH_DIMENSION as usize;
+pub const BASE_MISSING_VALUES: u8 = 30;
 
 ////////////////////////////////////////
 
 pub struct State {
-    pub grid: [[Option<u8>; LENGTH_USIZE]; LENGTH_USIZE],
+    pub grid_to_solve: Grid,
+    pub original_grid: Grid,
 
     pub cursor_row: usize,
     pub cursor_col: usize,
+
+    pub original_nb_missing_values: u8,
+    pub remaining_nb_missing_values: u8,
 }
 
 /////////////////
 
 impl State {
     pub fn new() -> State {
+        let grid = Grid::generate(Some(BASE_MISSING_VALUES));
+
         State {
-            grid: [[None; LENGTH_USIZE]; LENGTH_USIZE],
+            grid_to_solve: grid.clone(),
+            original_grid: grid.clone(),
 
             cursor_row: 0,
             cursor_col: 0,
+
+            original_nb_missing_values: BASE_MISSING_VALUES,
+            remaining_nb_missing_values: BASE_MISSING_VALUES,
         }
     }
 
     /////////////////
+    // Private
 
     pub fn move_cursor(&mut self, dr: i32, dc: i32) {
         let new_row = (self.cursor_row as i32 + dr).clamp(0, 8) as usize;
@@ -37,11 +53,28 @@ impl State {
 
     pub fn set_number(&mut self, num: u8) {
         if num >= 1 && num <= LENGTH_DIMENSION {
-            self.grid[self.cursor_row][self.cursor_col] = Some(num);
+            self.grid_to_solve.values[self.cursor_row][self.cursor_col] = num;
+
+            self.remaining_nb_missing_values -= 1;
         }
     }
 
     pub fn clear_cell(&mut self) {
-        self.grid[self.cursor_row][self.cursor_col] = None;
+        let original_value = self.original_grid.values[self.cursor_row][self.cursor_col];
+        self.grid_to_solve.values[self.cursor_row][self.cursor_col] = original_value;
+
+        self.remaining_nb_missing_values += 1;
     }
+
+    fn reset_state(&mut self) {
+        self.grid_to_solve = self.original_grid.clone();
+
+        self.cursor_row = 0;
+        self.cursor_col = 0;
+
+        self.remaining_nb_missing_values = self.original_nb_missing_values;
+    }
+
+    /////////////////
+    // Private
 }
--- src/view/grid.rs
+++ src/view/grid.rs
@@ -10,6 +10,15 @@ use crate::core::state::{State, LENGTH_USIZE};
 
 ////////////////////////////////////////
 
+const DARK_CELL_BG: Color = Color::Black;
+const LIGHT_CELL_BG: Color = Color::Rgb(15, 15, 15);
+const SELECTED_CELL_BG: Color = Color::Rgb(30, 30, 30);
+
+const TEXT_TO_FILL_FG: Color = Color::Yellow;
+const TEXT_STATIC_FG: Color = Color::White;
+
+////////////////////////////////////////
+
 pub fn render_grid(f: &mut Frame, state: &State, area: Rect) {
     // Create 9 rows
     let rows = Layout::default()
@@ -39,25 +48,30 @@ pub fn render_grid(f: &mut Frame, state: &State, area: Rect) {
         for col in 0..LENGTH_USIZE {
             let is_selected = state.cursor_row == row && state.cursor_col == col;
 
-            let cell_value = state.grid[row][col];
-            let text = if let Some(num) = cell_value {
-                num.to_string()
-            } else {
-                " ".to_string()
-            };
+            let cell_value_fillable = state.grid_to_solve.values[row][col];
+            let cell_value_original = state.original_grid.values[row][col];
+
+            let mut text = String::from("-");
+            if cell_value_fillable != 0 {
+                text = cell_value_fillable.to_string();
+            }
 
             if col == 3 || col == 6 {
                 col_should_black = !col_should_black;
             }
 
-            let mut style = Style::default().fg(Color::White);
+            // Text style
+            let mut style = Style::default().fg(TEXT_STATIC_FG);
+            if cell_value_original == 0 {
+                style = style.fg(TEXT_TO_FILL_FG)
+            }
             if is_selected {
-                style = style.bg(Color::Blue).add_modifier(Modifier::BOLD);
+                style = style.bg(SELECTED_CELL_BG).add_modifier(Modifier::BOLD);
             } else {
                 if !col_should_black {
-                    style = style.bg(Color::DarkGray);
+                    style = style.bg(LIGHT_CELL_BG);
                 } else {
-                    style = style.bg(Color::Black);
+                    style = style.bg(DARK_CELL_BG);
                 }
             }
 
